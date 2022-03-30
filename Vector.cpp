c648476f8995a795067c1e61a696b25a6e1a160c
dattali18 <danielattali16@gmail.com>
2022-03-30 20:00:09 +0300

    Initial commit

---
+++ Vector.cpp
@@ -0,0 +1,4 @@
+// Created by Daniel Attali on 3/30/22.
+
+
+#include "Vector.h"
---
+++ Vector.h
@@ -0,0 +1,13 @@
+// Created by Daniel Attali on 3/30/22.
+
+#ifndef MATHLIBRARY_VECTOR_H
+#define MATHLIBRARY_VECTOR_H
+
+
+class Vector {
+private:
+
+};
+
+
+#endif //MATHLIBRARY_VECTOR_H
